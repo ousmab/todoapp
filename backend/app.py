2a6a3f1b652e4f5ba4f3ab71b9a72fc805288f97
ousmane <aboousmane@gmail.com>
2023-11-06 04:44:40 +0100

    add Todo and User Models

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,50 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
 app = Flask(__name__)
 
-@app.route("/home")
-def home():
-    return "<h1>bonjour le monde</h1>"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db/todoAppManager.db"
+db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    profil_image = db.Column(db.String(120), nullable=True)
+    todos = db.relationship("Todo", backref=db.backref('user', lazy=True))
+
+    @staticmethod
+    def add(user):
+        #user = User(usernmane= ,email= , pass=)
+        try:
+            db.session.add(user)
+            db.session.commit()
+            return True
+        except Exception as e:
+            return False
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    done = db.Column(db.Boolean, default=False)
+    archived = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+with app.app_context():
+    db.create_all()
+
+
+
+
+@app.route('/register')
+def register():
+    pass
+    # recuperation des données
+    #validation des données
+    # - verification de l'emal , username, password , si le new user nexiste pas
+    #sauvegarde en BD
 
 
 if __name__ == "__main__":
