051a780f9318e621b3b49852af8892a98eb7acfb
ousmane <aboousmane@gmail.com>
2023-11-06 03:20:04 +0100

    initial commit

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/home")
+def home():
+    return "<h1>bonjour le monde</h1>"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3500)
